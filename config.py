65d9d9f3c36a0dfedb80c40f559945fe9b43af7c
Sihagbots03 <amitsihag0031@gmail.com>
2024-10-25 16:55:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ import os, re
 
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+API_ID = int(os.environ.get("API_ID", "29346781"))
+API_HASH = os.environ.get("API_HASH", "75fb004873db1864a09c71cd1307bfa8")
+OWNER = int(os.environ.get("OWNER", "5860332990"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "Trymrrobot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "SihagBots )
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002386658352"))
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://manojmanojs0014:amitsihag@cluster0.r2q9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
 
 STRING = os.environ.get("STRING", "")
