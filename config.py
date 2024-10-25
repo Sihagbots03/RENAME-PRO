d84b252433863e071daab04861e02cc21aa22ea7
Sihagbots03 <amitsihag0031@gmail.com>
2024-10-25 17:23:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_HASH = os.environ.get("API_HASH", "75fb004873db1864a09c71cd1307bfa8")
 OWNER = int(os.environ.get("OWNER", "5860332990"))
 BOT_USERNAME = os.environ.get('BOT_USERNAME', "Trymrrobot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "SihagBots )
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "SihagBots" )
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002386658352"))
 
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://manojmanojs0014:amitsihag@cluster0.r2q9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
